302dbdf4b3c5cd22f01b2bea769d10237ed46069
CREDSYSTEM\edson.sato <edson.sato@credsystem.com.br>
2023-10-02 20:45:03 -0300

    Inicializacao do banco com scripts

--- exemplo3-oracle/db-scripts/01_add_user.sql
+++ exemplo3-oracle/db-scripts/01_add_user.sql
@@ -1,3 +1,14 @@
+--declare
+--userexist integer;
+--begin
+--  select count(*) into userexist from dba_users where username='SATO';
+--  if (userexist = 0) then
+--    execute immediate 'create user sato identified by abc123';
+--    DEFAULT TABLESPACE users;
+--    TEMPORARY TABLESPACE temp;
+--    GRANT ALL PRIVILEGES TO sato;  end if;
+--end;
+
 CREATE USER sato IDENTIFIED BY abc123
 DEFAULT TABLESPACE users
 TEMPORARY TABLESPACE temp;
